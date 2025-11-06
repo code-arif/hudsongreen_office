e19367a783bef9fcc081bab50baea1db23344094
code-arif <arifulislam6460@gmail.com>
2025-11-06 17:12:54 +0600

    chore(routes): add comment to dashboard data route

--- routes/backend.php
+++ routes/backend.php
@@ -15,7 +15,7 @@ use App\Http\Controllers\Web\Backend\Settings\SettingController;
 
 Route::middleware(['auth', 'admin'])->group(function () {
     Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
-    Route::get('dashboard/data', [DashboardController::class, 'getDashboardData'])->name('dashboard.data');
+    Route::get('dashboard/data', [DashboardController::class, 'getDashboardData'])->name('dashboard.data'); // working
 
     // employee manage
     Route::prefix('employee')->name('employee.')->group(function () {
